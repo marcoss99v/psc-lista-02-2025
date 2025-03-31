93b895927133442f1bb72ece65c90fb722e6e9f9
marcoss99v <marcosss2305@gmail.com>
2025-03-31 00:03:19 -0300

    Add files via upload

---
+++ Exercicio 5 Viagem.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Valor para a Alemanha: R$ ");
+        double precoAlemanha = scanner.nextDouble();
+        System.out.print("Valor para Portugal: R$ ");
+        double precoPortugal = scanner.nextDouble();
+        System.out.print("Valor para a Itália: R$ ");
+        double precoItalia = scanner.nextDouble();
+        System.out.print("Quantidade de pessoas para Alemanha: ");
+        int pessoasAlemanha = scanner.nextInt();
+        System.out.print("Quantidade de pessoas para Portugal: ");
+        int pessoasPortugal = scanner.nextInt();
+        System.out.print("Quantidade de pessoas para Itália: ");
+        int pessoasItalia = scanner.nextInt();
+        double totalAlemanha = precoAlemanha * pessoasAlemanha;
+        double totalPortugal = precoPortugal * pessoasPortugal;
+        double totalItalia = precoItalia * pessoasItalia;
+
+        double valorTotal = totalAlemanha + totalPortugal + totalItalia;
+        System.out.printf("O valor total da Eurotrip é: R$ %.2f%n", valorTotal);
+
+        scanner.close();
+    }
+}
